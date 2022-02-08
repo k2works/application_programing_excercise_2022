f2af27de34a8c3d1eb1b2ec40ed2eb3e90b4d107
k2works <kakimomokkuri@gmail.com>
2022-02-08 19:11:36 +0900

    fix: メモ化

--- api/domain/fibonacci.ts
+++ api/domain/fibonacci.ts
@@ -32,7 +32,7 @@ export class FibonacciRecursive implements Command {
     if (memo[number]) return memo[number];
     if (number === 0) return 0;
     if (number === 1) return 1;
-    memo[number] = this.exec(number - 1) + this.exec(number - 2);
+    memo[number] = this.exec(number - 1, memo) + this.exec(number - 2, memo);
     return memo[number];
   }
 }
