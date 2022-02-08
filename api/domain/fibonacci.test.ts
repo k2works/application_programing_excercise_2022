8478bbe0a14dcf0cb0feec8ca0b27b56e9140ab5
k2works <kakimomokkuri@gmail.com>
2022-02-08 18:32:20 +0900

    refactor: テストケース整理

--- api/domain/fibonacci.test.ts
+++ api/domain/fibonacci.test.ts
@@ -15,31 +15,35 @@ describe("フィボナッチ数列", () => {
     loop = new Fibonacci(new FibonacciLoop());
     generalTerm = new Fibonacci(new FibonacciGenralTerm());
   });
-  test.each([
-    [0, 0],
-    [1, 1],
-    [2, 1],
-    [3, 2],
-    [4, 3],
-    [5, 5],
-  ])("%iを渡した%pを返す", (number: number, expected: number) => {
-    expect(recursive.exec(number)).toEqual(expected);
+  describe("正常系", () => {
+    test.each([
+      [0, 0],
+      [1, 1],
+      [2, 1],
+      [3, 2],
+      [4, 3],
+      [5, 5],
+    ])("%iを渡した%pを返す", (number: number, expected: number) => {
+      expect(recursive.exec(number)).toEqual(expected);
+    });
+
+    test("大きな数値で計算する(再帰による実装)", () => {
+      expect(recursive.exec(40)).toEqual(102334155);
+    });
+
+    test("大きな数値で計算する(ループによる実装)", () => {
+      expect(loop.exec(40)).toEqual(102334155);
+    });
+
+    test("大きな数値で計算する(一般項による実装)", () => {
+      expect(generalTerm.exec(40)).toEqual(102334155);
+    });
   });
 
-  test("大きな数値で計算する(再帰による実装)", () => {
-    expect(recursive.exec(40)).toEqual(102334155);
-  });
-
-  test("大きな数値で計算する(ループによる実装)", () => {
-    expect(loop.exec(40)).toEqual(102334155);
-  });
-
-  test("大きな数値で計算する(一般項による実装)", () => {
-    expect(generalTerm.exec(40)).toEqual(102334155);
-  });
-
-  test("マイナスの値はエラーを返す", () => {
-    const result = () => recursive.exec(-1);
-    expect(result).toThrowError("マイナスの値は指定できません");
+  describe("異常系", () => {
+    test("マイナスの値はエラーを返す", () => {
+      const result = () => recursive.exec(-1);
+      expect(result).toThrowError("マイナスの値は指定できません");
+    });
   });
 });
