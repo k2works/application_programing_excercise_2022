8f8a7fbfb988a9e0d060dc9450510e3366554094
k2works <kakimomokkuri@gmail.com>
2022-02-08 20:09:15 +0900

    feat: FibonacciApiClient

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { View } from "./src/View";
 import { setUp } from "./src/Dev.js";
-const view = new View();
-view.render();
-setUp();
+
+let view;
+if (process.env.NODE_ENV === "production") {
+  view = new View({ apiUrl: "/api" });
+  view.render();
+  setUp();
+} else {
+  view = new View({ apiUrl: "http://localhost:3000/api" });
+  view.render();
+  setUp();
+}
