cd635e03ef89d89a4a2ba2db02ef53d8c9bd3d2f
k2works <kakimomokkuri@gmail.com>
2022-02-10 17:49:43 +0900

    test: Destroy

--- src/App.test.js
+++ src/App.test.js
@@ -136,4 +136,20 @@ describe("TodoItemRepositoryを利用するサンプルコード", () => {
       });
     });
   });
+
+
+  test("Destroy", () => {
+    const todoListModel = new TodoListModel();
+    [...Array(10).keys()]
+      .map((i) => new TodoItemModel({ title: `Todo${i}`, completed: false }))
+      .map((j) => todoListModel.addTodo(j));
+
+    return repository.createBatch(todoListModel.getTodoItems()).then(() => {
+      return repository.destroy().then(() => {
+        return repository.selectAll().then((result) => {
+          expect(result).toEqual([]);
+        });
+      });
+    });
+  });
 });
