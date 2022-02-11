626b0909e21af6ec5499e32067ef22518418dd67
k2works <kakimomokkuri@gmail.com>
2022-02-11 19:17:01 +0900

    fix: webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -15,4 +15,18 @@ module.exports = {
       filename: "index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { url: false },
+          },
+        ],
+      },
+    ],
+  },
 };
