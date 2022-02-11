dfa68410a3d2e0b7d647dad9d6162c9184c7c521
k2works <kakimomokkuri@gmail.com>
2022-02-11 19:04:39 +0900

    test: やること

--- api/Todo.test.ts
+++ api/Todo.test.ts
@@ -1,8 +1,55 @@
-import { Todo } from "./Todo";
+import { getManager, getRepository } from "typeorm";
+import connection from "./connection";
+import { Todo } from "./entity/Todo";
 
-describe("Todo", () => {
-  it("挨拶する", () => {
-    const todo = new Todo();
-    expect(todo.greeting()).toEqual("hello world");
-  });
+beforeAll(async () => {
+  await connection.create();
+});
+
+afterAll(async () => {
+  await connection.close();
+});
+
+beforeEach(async () => {
+  await connection.clear();
+});
+
+it("やることを作成する", async () => {
+  const todo = new Todo();
+  todo.title = "タイトル";
+  todo.completed = false;
+
+  let repository = getRepository(Todo);
+  await repository.save(todo);
+  expect(todo.id).toBeDefined();
+  let result = await repository.findOne();
+  expect(result?.title).toBe("タイトル");
+  expect(result?.completed).toBe(false);
+});
+
+it("やることを更新する", async () => {
+  const todo = new Todo();
+  todo.title = "タイトル";
+  todo.completed = false;
+
+  let repository = getRepository(Todo);
+  await repository.save(todo);
+  let result = await repository.find();
+  result[0].title = "タイトル2";
+  await repository.save(result[0]);
+  result = await repository.find();
+  expect(result[0].title).toBe("タイトル2");
+});
+
+it("やることを削除する", async () => {
+  const todo = new Todo();
+  todo.title = "タイトル";
+  todo.completed = false;
+
+  let repository = getRepository(Todo);
+  await repository.save(todo);
+  let result = await repository.find();
+  await repository.remove(result[0]);
+  result = await repository.find();
+  expect(result.length).toBe(0);
 });
