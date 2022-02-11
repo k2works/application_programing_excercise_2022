55abb7a43ca73d1a24076be74f167993d2ada7f5
k2works <kakimomokkuri@gmail.com>
2022-02-11 19:35:58 +0900

    test: やること

--- api/Todo.test.ts
+++ api/Todo.test.ts
@@ -1,55 +1,15 @@
-import { getManager, getRepository } from "typeorm";
-import connection from "./connection";
-import { Todo } from "./entity/Todo";
+import { Todo } from "./Todo";
 
-beforeAll(async () => {
-  await connection.create();
-});
+describe("Todo", () => {
+  it("やることを生成する", () => {
+    const todo = new Todo("タイトル");
+    expect(todo.getTitle()).toBe("タイトル");
+    expect(todo.getCompleted()).toBe(false);
+  });
 
-afterAll(async () => {
-  await connection.close();
-});
-
-beforeEach(async () => {
-  await connection.clear();
-});
-
-it("やることを作成する", async () => {
-  const todo = new Todo();
-  todo.title = "タイトル";
-  todo.completed = false;
-
-  let repository = getRepository(Todo);
-  await repository.save(todo);
-  expect(todo.id).toBeDefined();
-  let result = await repository.findOne();
-  expect(result?.title).toBe("タイトル");
-  expect(result?.completed).toBe(false);
-});
-
-it("やることを更新する", async () => {
-  const todo = new Todo();
-  todo.title = "タイトル";
-  todo.completed = false;
-
-  let repository = getRepository(Todo);
-  await repository.save(todo);
-  let result = await repository.find();
-  result[0].title = "タイトル2";
-  await repository.save(result[0]);
-  result = await repository.find();
-  expect(result[0].title).toBe("タイトル2");
-});
-
-it("やることを削除する", async () => {
-  const todo = new Todo();
-  todo.title = "タイトル";
-  todo.completed = false;
-
-  let repository = getRepository(Todo);
-  await repository.save(todo);
-  let result = await repository.find();
-  await repository.remove(result[0]);
-  result = await repository.find();
-  expect(result.length).toBe(0);
+  it("やることを完了する", () => {
+    const todo = new Todo("タイトル");
+    todo.complete();
+    expect(todo.getCompleted()).toBe(true);
+  });
 });
--- api/Todo.ts
+++ api/Todo.ts
@@ -1,5 +1,21 @@
 export class Todo {
-  greeting(): string {
-    return "hello world";
+  private title: string;
+  private completed: boolean;
+
+  getTitle(): string {
+    return this.title;
+  }
+
+  getCompleted(): boolean {
+    return this.completed;
+  }
+
+  constructor(title: string) {
+    this.title = title;
+    this.completed = false;
+  }
+
+  public complete(): void {
+    this.completed = true;
   }
 }
