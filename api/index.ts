31a81df906a49a00b423eec6770c5a8c76e62dc9
k2works <kakimomokkuri@gmail.com>
2022-02-12 13:45:46 +0900

    fix: API

--- api/index.ts
+++ api/index.ts
@@ -4,8 +4,8 @@ const app = express();
 
 app.get("/api", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  const todo = new Todo();
-  res.send(todo.greeting());
+  const todo = new Todo("hello World");
+  res.send(todo.getTitle());
 });
 
 export default app;
