a23791e96a1b08aaefe163d85470deba889cdb63
k2works <kakimomokkuri@gmail.com>
2022-02-14 18:08:18 +0900

    feat(WIP): ApiClient

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { Todo } from "./domain/Todo";
 import { createConnection } from "typeorm";
 import { TodoService } from "./application/TodoService";
 
 const app = express();
-const service = new TodoService();
-
+app.use(cors({ origin: true, credentials: true }));
 app.use(
   express.urlencoded({
     extended: true,
@@ -22,22 +22,29 @@ interface TodoRequest {
   dueDate: Date | null;
   id: number | null;
 }
+const service = new TodoService();
 
 createConnection()
   .then(async (connection) => {
     app.get("/api", async (req, res) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
       const result = await service.selectAll();
       res.send(result);
     });
 
     app.post("/api", async (req, res) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
       const request: TodoRequest = JSON.parse(req.body);
       const todo = new Todo(request.title, request.completed);
       await service.create(todo);
-      const result = await service.selectAll();
-      res.send(result);
+      res.end();
+    });
+
+    app.delete("/api", async (req, res) => {
+      const data = JSON.parse(JSON.stringify(req.body));
+      if (data.id !== null) {
+        const todo = await service.find(parseInt(data.id));
+        await service.delete(todo);
+      }
+      res.end();
     });
   })
   .catch((error) => console.log(error));
