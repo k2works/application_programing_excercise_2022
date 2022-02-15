7a14729d5e73bdf3c3f438d811d255025fab9dff
k2works <kakimomokkuri@gmail.com>
2022-02-15 16:56:26 +0900

    refactor: テストコードの整理

--- api/application/TodoService.test.ts
+++ api/application/TodoService.test.ts
@@ -16,74 +16,77 @@ beforeEach(async () => {
 });
 
 describe("TodoService", () => {
-  it("やることを作成する", async () => {
-    const servce = new TodoService();
-    const params: TodoRequest = setupParams();
-    await servce.create(params);
-    const result = await servce.selectAll();
-    expect(result.Value[0].Title).toBe("タイトル");
+  function setupParams(
+    title: string = "タイトル",
+    id: number | null = null
+  ): TodoRequest {
+    const todo = new Todo(title);
+    const params: TodoRequest = {
+      title: todo.Title,
+      completed: todo.Completed,
+      createdAt: todo.CreatedAt,
+      completedAt: todo.CompletedAt,
+      dueDate: todo.DueDate,
+      id: id,
+    };
+    return params;
+  }
+  describe("正常系", () => {
+    it("やることを作成する", async () => {
+      const servce = new TodoService();
+      const params: TodoRequest = setupParams();
+      await servce.create(params);
+      const result = await servce.selectAll();
+      expect(result.Value[0].Title).toBe("タイトル");
+    });
+
+    it("やることを検索する", async () => {
+      const servce = new TodoService();
+      const params: TodoRequest = setupParams();
+      await servce.create(params);
+      if (params.id !== null) {
+        const result = await servce.find(params.id);
+        expect(result.Title).toBe("タイトル");
+      }
+    });
+
+    it("やることを更新する", async () => {
+      const servce = new TodoService();
+      const params: TodoRequest = setupParams();
+      await servce.create(params);
+      let result = await servce.selectAll();
+
+      const id = result.Value[0].Id;
+      if (id !== null) {
+        const params2: TodoRequest = setupParams("タイトル2", id);
+        await servce.update(params2);
+        result = await servce.selectAll();
+      }
+      expect(result.Value[0].Title).toBe("タイトル2");
+    });
+
+    it("やることを削除する", async () => {
+      const servce = new TodoService();
+      const params: TodoRequest = setupParams();
+      await servce.create(params);
+      let result = await servce.selectAll();
+      if (result.Value[0].Id !== null) {
+        await servce.delete(result.Value[0].Id);
+        result = await servce.selectAll();
+        expect(result.Value.length).toBe(0);
+      }
+    });
+
+    it("やることの件数を取得する", async () => {
+      const servce = new TodoService();
+      const params: TodoRequest = setupParams();
+      await servce.create(params);
+      if (params.id !== null) {
+        const result = await servce.count();
+        expect(result).toBe(1);
+      }
+    });
   });
 
-  it("やることを検索する", async () => {
-    const servce = new TodoService();
-    const params: TodoRequest = setupParams();
-    await servce.create(params);
-    if (params.id !== null) {
-      const result = await servce.find(params.id);
-      expect(result.Title).toBe("タイトル");
-    }
-  });
-
-  it("やることを更新する", async () => {
-    const servce = new TodoService();
-    const params: TodoRequest = setupParams();
-    await servce.create(params);
-    let result = await servce.selectAll();
-
-    const id = result.Value[0].Id;
-    if (id !== null) {
-      const params2: TodoRequest = setupParams("タイトル2", id);
-      await servce.update(params2);
-      result = await servce.selectAll();
-    }
-    expect(result.Value[0].Title).toBe("タイトル2");
-  });
-
-  it("やることを削除する", async () => {
-    const servce = new TodoService();
-    const params: TodoRequest = setupParams();
-    await servce.create(params);
-    let result = await servce.selectAll();
-    if (result.Value[0].Id !== null) {
-      await servce.delete(result.Value[0].Id);
-      result = await servce.selectAll();
-      expect(result.Value.length).toBe(0);
-    }
-  });
-
-  it("やることの件数を取得する", async () => {
-    const servce = new TodoService();
-    const params: TodoRequest = setupParams();
-    await servce.create(params);
-    if (params.id !== null) {
-      const result = await servce.count();
-      expect(result).toBe(1);
-    }
-  });
+  describe("異常系", () => {});
 });
-
-function setupParams(
-  title: string = "タイトル",
-  id: number | null = null
-): TodoRequest {
-  const todo = new Todo(title);
-  const params: TodoRequest = {
-    title: todo.Title,
-    completed: todo.Completed,
-    createdAt: todo.CreatedAt,
-    completedAt: todo.CompletedAt,
-    dueDate: todo.DueDate,
-    id: id,
-  };
-  return params;
-}
