8955be225a680bb48698ede366b89d19cefbcf9a
k2works <kakimomokkuri@gmail.com>
2022-02-15 17:03:32 +0900

    refactor: 不要なコードの削除

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import "./src/style.css";
 import { App } from "./src/App.js";
 import { setUp } from "./src/Dev.js";
-import { TodoItemRepository } from "./src/application/TodoItemRepository";
 
-const repository = new TodoItemRepository("todo_test", "todo_items");
-repository.setup().then(() => {
-  const app = new App();
-  app.mount();
-});
+const app = new App();
+app.mount();
 setUp();
