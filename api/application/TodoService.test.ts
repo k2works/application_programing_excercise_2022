df7fddedbce2ca8e7dffe6adf06353f3eb33cd99
k2works <kakimomokkuri@gmail.com>
2022-02-15 16:52:25 +0900

    refactor: メソッドの抽出

--- api/application/TodoService.test.ts
+++ api/application/TodoService.test.ts
@@ -1,7 +1,4 @@
 import connection from "../utils/connection";
-import { CompletedAt } from "../domain/CompletedAt";
-import { CreatedAt } from "../domain/CreatedAt";
-import { DueDate } from "../domain/DueDate";
 import { Todo } from "../domain/Todo";
 import { TodoService } from "./TodoService";
 import { TodoRequest } from "..";
@@ -21,15 +18,7 @@ beforeEach(async () => {
 describe("TodoService", () => {
   it("やることを作成する", async () => {
     const servce = new TodoService();
-    const todo = new Todo("タイトル");
-    const params: TodoRequest = {
-      title: todo.Title,
-      completed: todo.Completed,
-      createdAt: todo.CreatedAt,
-      completedAt: todo.CompletedAt,
-      dueDate: todo.DueDate,
-      id: todo.Id,
-    };
+    const params: TodoRequest = setupParams();
     await servce.create(params);
     const result = await servce.selectAll();
     expect(result.Value[0].Title).toBe("タイトル");
@@ -37,55 +26,23 @@ describe("TodoService", () => {
 
   it("やることを検索する", async () => {
     const servce = new TodoService();
-    const todo = new Todo("タイトル");
-    const params: TodoRequest = {
-      title: todo.Title,
-      completed: todo.Completed,
-      createdAt: todo.CreatedAt,
-      completedAt: todo.CompletedAt,
-      dueDate: todo.DueDate,
-      id: todo.Id,
-    };
+    const params: TodoRequest = setupParams();
     await servce.create(params);
-    if (todo.Id !== null) {
-      const result = await servce.find(todo.Id);
+    if (params.id !== null) {
+      const result = await servce.find(params.id);
       expect(result.Title).toBe("タイトル");
     }
   });
 
   it("やることを更新する", async () => {
     const servce = new TodoService();
-    const todo = new Todo("タイトル");
-    const params: TodoRequest = {
-      title: todo.Title,
-      completed: todo.Completed,
-      createdAt: todo.CreatedAt,
-      completedAt: todo.CompletedAt,
-      dueDate: todo.DueDate,
-      id: todo.Id,
-    };
+    const params: TodoRequest = setupParams();
     await servce.create(params);
     let result = await servce.selectAll();
 
     const id = result.Value[0].Id;
     if (id !== null) {
-      const todo2 = new Todo(
-        "タイトル2",
-        true,
-        new CreatedAt(new Date()),
-        new CompletedAt(null),
-        new DueDate(null),
-        id
-      );
-
-      const params2: TodoRequest = {
-        title: todo2.Title,
-        completed: todo2.Completed,
-        createdAt: todo2.CreatedAt,
-        completedAt: todo2.CompletedAt,
-        dueDate: todo2.DueDate,
-        id: todo2.Id,
-      };
+      const params2: TodoRequest = setupParams("タイトル2", id);
       await servce.update(params2);
       result = await servce.selectAll();
     }
@@ -94,15 +51,7 @@ describe("TodoService", () => {
 
   it("やることを削除する", async () => {
     const servce = new TodoService();
-    const todo = new Todo("タイトル");
-    const params: TodoRequest = {
-      title: todo.Title,
-      completed: todo.Completed,
-      createdAt: todo.CreatedAt,
-      completedAt: todo.CompletedAt,
-      dueDate: todo.DueDate,
-      id: todo.Id,
-    };
+    const params: TodoRequest = setupParams();
     await servce.create(params);
     let result = await servce.selectAll();
     if (result.Value[0].Id !== null) {
@@ -114,19 +63,27 @@ describe("TodoService", () => {
 
   it("やることの件数を取得する", async () => {
     const servce = new TodoService();
-    const todo = new Todo("タイトル");
-    const params: TodoRequest = {
-      title: todo.Title,
-      completed: todo.Completed,
-      createdAt: todo.CreatedAt,
-      completedAt: todo.CompletedAt,
-      dueDate: todo.DueDate,
-      id: todo.Id,
-    };
+    const params: TodoRequest = setupParams();
     await servce.create(params);
-    if (todo.Id !== null) {
+    if (params.id !== null) {
       const result = await servce.count();
       expect(result).toBe(1);
     }
   });
 });
+
+function setupParams(
+  title: string = "タイトル",
+  id: number | null = null
+): TodoRequest {
+  const todo = new Todo(title);
+  const params: TodoRequest = {
+    title: todo.Title,
+    completed: todo.Completed,
+    createdAt: todo.CreatedAt,
+    completedAt: todo.CompletedAt,
+    dueDate: todo.DueDate,
+    id: id,
+  };
+  return params;
+}
