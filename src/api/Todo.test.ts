8a348b6e8c13a295b8a6a73c8ca187aa6a46b310
k2works <kakimomokkuri@gmail.com>
2022-02-17 14:57:14 +0900

    feat: 追加仕様

--- src/api/Todo.test.ts
+++ src/api/Todo.test.ts
@@ -18,4 +18,29 @@ describe("Todo", () => {
     const todo2 = new Todo("タイトル");
     expect(todo1.equals(todo2)).toBe(true);
   });
+
+    it("やることを生成したら開始日を設定する", () => {
+      const todo = new Todo("タイトル");
+      expect(todo.CreatedAt).toBeDefined();
+    });
+
+    it("やることを更新したら完了日を設定する", () => {
+      const todo = new Todo("タイトル");
+      todo.complete();
+      expect(todo.CompletedAt).toBeDefined();
+    });
+
+    it("やることに期限を設定する", () => {
+      const todo = new Todo("タイトル");
+      todo.DueDate = new Date();
+      expect(todo.DueDate).toBeDefined();
+    });
+
+    it("やることの期限を確認する", () => {
+      const todo = new Todo("タイトル");
+      const due = new Date();
+      due.setDate(due.getDate() - 1);
+      todo.DueDate = due;
+      expect(todo.overDue()).toBe(true);
+    });
 });
--- src/api/Todo.ts
+++ src/api/Todo.ts
@@ -1,6 +1,9 @@
 export class Todo {
   private title: Title;
   private completed: boolean;
+  private createdAt: Date;
+  private completedAt: Date | null;
+  private dueDate: Date | null;
 
   get Title(): string {
     return this.title.Value;
@@ -10,17 +13,49 @@ export class Todo {
     return this.completed;
   }
 
+  get CreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  get CompletedAt(): Date | null {
+    return this.completedAt;
+  }
+
+  get DueDate(): Date | null {
+    return this.dueDate;
+  }
+
+  set DueDate(date: Date | null) {
+    this.dueDate = date;
+  }
+
   constructor(title: string) {
     this.title = new Title(title);
     this.completed = false;
+    this.createdAt = new Date();
+    this.completedAt = null;
+    this.dueDate = null;
   }
 
   public complete(): void {
     this.completed = true;
   }
 
+  public overDue(): boolean {
+    if (this.dueDate === null) {
+      return false;
+    }
+    return this.createdAt.getTime() > this.dueDate.getTime();
+  }
+
   public equals(other: Todo): boolean {
-    return this.title.equals(other.title) && this.completed === other.completed;
+    return (
+      this.title.equals(other.title) &&
+      this.completed === other.completed &&
+      this.createdAt === other.createdAt &&
+      this.completedAt === other.completedAt &&
+      this.dueDate === other.dueDate
+    );
   }
 }
 
