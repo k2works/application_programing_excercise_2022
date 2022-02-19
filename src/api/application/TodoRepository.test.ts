f67e4153de2da25fb4f836492d18f8783776f853
k2works <kakimomokkuri@gmail.com>
2022-02-19 11:36:43 +0900

    refactor: テストコードの文書化

--- src/api/application/TodoRepository.test.ts
+++ src/api/application/TodoRepository.test.ts
@@ -18,44 +18,48 @@ describe("TodoRepository", () => {
     await connection.clear();
   });
 
-  it("やることを作成する", async () => {
-    const repository = new TodoRepository();
-    const todo = new Todo("タイトル");
-    await repository.addTodo(todo);
-    const result = await repository.getTodos();
-    expect(result.Value[0].Title).toBe("タイトル");
-  });
+  describe("正常系", () => {
+    it("やることを作成する", async () => {
+      const repository = new TodoRepository();
+      const todo = new Todo("タイトル");
+      await repository.addTodo(todo);
+      const result = await repository.getTodos();
+      expect(result.Value[0].Title).toBe("タイトル");
+    });
 
-  it("やることを更新する", async () => {
-    const repository = new TodoRepository();
-    const todo = new Todo("タイトル");
-    await repository.addTodo(todo);
-    let result = await repository.getTodos();
+    it("やることを更新する", async () => {
+      const repository = new TodoRepository();
+      const todo = new Todo("タイトル");
+      await repository.addTodo(todo);
+      let result = await repository.getTodos();
 
-    const id = result.Value[0].Id;
-    if (id !== null) {
-      const todo2 = new Todo(
-        "タイトル2",
-        true,
-        new CreatedAt(new Date()),
-        new CompletedAt(null),
-        new DueDate(null),
-        id
-      );
+      const id = result.Value[0].Id;
+      if (id !== null) {
+        const todo2 = new Todo(
+          "タイトル2",
+          true,
+          new CreatedAt(new Date()),
+          new CompletedAt(null),
+          new DueDate(null),
+          id
+        );
 
-      await repository.updateTodo(todo2);
+        await repository.updateTodo(todo2);
+        result = await repository.getTodos();
+      }
+      expect(result.Value[0].Title).toBe("タイトル2");
+    });
+
+    it("やることを削除する", async () => {
+      const repository = new TodoRepository();
+      const todo = new Todo("タイトル");
+      await repository.addTodo(todo);
+      let result = await repository.getTodos();
+      await repository.deleteTodo(result.Value[0]);
       result = await repository.getTodos();
-    }
-    expect(result.Value[0].Title).toBe("タイトル2");
+      expect(result.Value.length).toBe(0);
+    });
   });
 
-  it("やることを削除する", async () => {
-    const repository = new TodoRepository();
-    const todo = new Todo("タイトル");
-    await repository.addTodo(todo);
-    let result = await repository.getTodos();
-    await repository.deleteTodo(result.Value[0]);
-    result = await repository.getTodos();
-    expect(result.Value.length).toBe(0);
-  });
+  describe("異常系", () => {});
 });
