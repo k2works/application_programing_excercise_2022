93766da39742aa0dd0cacceb0a1df292a6e6a17b
k2works <kakimomokkuri@gmail.com>
2022-02-25 18:13:43 +0900

    test: 非同期API連家

--- src/client/components/TodoListView.tsx
+++ src/client/components/TodoListView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { TodoItemView } from "./TodoItemView";
 
 export const TodoListView: React.FC = () => {
@@ -7,9 +7,10 @@ export const TodoListView: React.FC = () => {
     { title: "b", status: "未着手", id: 0, completed: true, duDate: "" },
     { title: "a", status: "未着手", id: 0, completed: false, duDate: "" },
   ];
+  const [todoList, setTodoList] = useState(items);
 
   useEffect(() => {
-    const getApi = (url: string) => {
+    const getApi = async (url: string) => {
       const service = (
         resolve: (value?: string) => void,
         reject: (reason?: any) => void
@@ -25,16 +26,35 @@ export const TodoListView: React.FC = () => {
       };
       return new Promise(service);
     };
-
-    const result = getApi("http://localhost:3000/api/todos");
-    result.then((data: any) => {
-      console.log(data.value);
-    });
+    (async () => {
+      const result: any = await getApi("http://localhost:3000/api/todos");
+      const resultList = result.value;
+      console.log(resultList);
+      let itemList: any[] = [];
+      let mapResult = {};
+      resultList.forEach((item: any) => {
+        mapResult = {
+          title: item.title.value,
+          status: item.status.value,
+          id: item.id,
+          completed: item.isCompleted,
+          duDate: item.dueDate.value,
+        };
+        itemList.push(mapResult);
+      });
+      console.log(itemList);
+      const items = [
+        { title: "a", status: "未着手", id: 0, completed: false, duDate: "" },
+        { title: "b", status: "未着手", id: 0, completed: true, duDate: "" },
+        { title: "a", status: "未着手", id: 0, completed: false, duDate: "" },
+      ];
+      setTodoList(itemList);
+    })();
   }, []);
 
   return (
     <ul>
-      {items.map((item) => (
+      {todoList.map((item) => (
         <TodoItemView
           title={item.title}
           status={item.status}
