264ba788e5a5465e3b461c1f7377527c36936564
k2works <kakimomokkuri@gmail.com>
2022-02-25 17:25:46 +0900

    test: 非同期API連携

--- src/client/components/TodoListView.tsx
+++ src/client/components/TodoListView.tsx
@@ -8,6 +8,28 @@ export const TodoListView: React.FC = () => {
     { title: "a", status: "未着手", id: 0, completed: false, duDate: "" },
   ];
 
+  const getApi = (url: string) => {
+    const service = (
+      resolve: (value?: string) => void,
+      reject: (reason?: any) => void
+    ) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          return resolve(json);
+        })
+        .catch((error) => {
+          return reject(error);
+        });
+    };
+    return new Promise(service);
+  };
+
+  const result = getApi("http://localhost:3000/api/todos");
+  result.then((data: any) => {
+    console.log(data.value);
+  });
+
   return (
     <ul>
       {items.map((item) => (
