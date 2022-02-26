4975565d725014f5b7e5bca193dadcdbbf13c86f
k2works <kakimomokkuri@gmail.com>
2022-02-26 16:11:21 +0900

    refactor: 関数の抽出

--- src/client/components/TodoListView.tsx
+++ src/client/components/TodoListView.tsx
@@ -3,49 +3,57 @@ import { TodoItemCountView } from "./TodoItemCountView";
 import { TodoItemView } from "./TodoItemView";
 
 export const TodoListView: React.FC = () => {
-  const items = [
-    { title: "", status: "", id: 0, completed: false, dueDate: "" },
-  ];
-  const [todoList, setTodoList] = useState(items);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const useTodoListApi = (url: string) => {
+    const items = [
+      { title: "", status: "", id: 0, completed: false, dueDate: "" },
+    ];
+    const [todoList, setTodoList] = useState(items);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
-  useEffect(() => {
-    const getApi = async (url: string) => {
-      const service = (
-        resolve: (value?: string) => void,
-        reject: (reason?: any) => void
-      ) => {
-        fetch(url)
-          .then((response) => response.json())
-          .then((json) => {
-            return resolve(json);
-          })
-          .catch((error) => {
-            return reject(error);
-          });
+    useEffect(() => {
+      const getApi = async (url: string) => {
+        const service = (
+          resolve: (value?: string) => void,
+          reject: (reason?: any) => void
+        ) => {
+          fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+              return resolve(json);
+            })
+            .catch((error) => {
+              return reject(error);
+            });
+        };
+        return new Promise(service);
       };
-      return new Promise(service);
-    };
 
-    (async () => {
-      setIsLoading(true);
-      try {
-        const result: any = await getApi("http://localhost:3000/api/todos");
-        const items = result.value.map((item: any) => ({
-          title: item.title.value,
-          status: item.status.value,
-          id: item.id,
-          completed: item.isCompleted,
-          dueDate: item.dueDate.value,
-        }));
-        setTodoList(items);
-      } catch (error) {
-        setIsError(true);
-      }
-      setIsLoading(false);
-    })();
-  }, []);
+      (async () => {
+        setIsLoading(true);
+        try {
+          const result: any = await getApi(url);
+          const items = result.value.map((item: any) => ({
+            title: item.title.value,
+            status: item.status.value,
+            id: item.id,
+            completed: item.isCompleted,
+            dueDate: item.dueDate.value,
+          }));
+          setTodoList(items);
+        } catch (error) {
+          setIsError(true);
+        }
+        setIsLoading(false);
+      })();
+    }, [url]);
+
+    return [{ todoList, isLoading, isError }];
+  };
+
+  const [{ todoList, isLoading, isError }] = useTodoListApi(
+    "http://localhost:3000/api/todos"
+  );
 
   return (
     <div>
