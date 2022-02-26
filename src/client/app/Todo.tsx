c111cd17cca23274e4f027dbcee50a6e827a5ce2
k2works <kakimomokkuri@gmail.com>
2022-02-26 17:33:47 +0900

    refactor: 関数の移動

--- src/client/app/Todo.tsx
+++ src/client/app/Todo.tsx
@@ -1,10 +1,97 @@
-import * as React from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { TodoInputView } from "../components/TodoInputView";
+import { TodoItemCountView } from "../components/TodoItemCountView";
 import { TodoListView } from "../components/TodoListView";
 
-export const Todo: React.FC = () => (
-  <div>
-    <TodoInputView></TodoInputView>
-    <TodoListView></TodoListView>
-  </div>
-);
+const dataFetchReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, isError: false };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "FETCH_FAILURE":
+      return { ...state, isLoading: false, isError: true };
+    default:
+      throw new Error();
+  }
+};
+
+const useTodoListApi = (url: string, initialData: any) => {
+  const [state, dispatch] = React.useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
+
+  React.useEffect(() => {
+    let didCancel = false;
+
+    const getApi = async (url: string) => {
+      const service = (
+        resolve: (value?: string) => void,
+        reject: (reason?: any) => void
+      ) => {
+        fetch(url)
+          .then((response) => response.json())
+          .then((json) => {
+            return resolve(json);
+          })
+          .catch((error) => {
+            return reject(error);
+          });
+      };
+      return new Promise(service);
+    };
+
+    (async () => {
+      dispatch({ type: "FETCH_INIT" });
+
+      try {
+        const result: any = await getApi(url);
+        const items = result.value.map((item: any) => ({
+          title: item.title.value,
+          status: item.status.value,
+          id: item.id,
+          completed: item.isCompleted,
+          dueDate: item.dueDate.value,
+        }));
+        dispatch({ type: "FETCH_SUCCESS", payload: items });
+      } catch (error) {
+        if (!didCancel) dispatch({ type: "FETCH_FAILURE" });
+      }
+    })();
+
+    return () => {
+      didCancel = true;
+    };
+  }, []);
+
+  return [state];
+};
+
+export const Todo: React.FC = () => {
+  const items = [
+    { title: "", status: "", id: 0, completed: false, dueDate: "" },
+  ];
+  const [state] = useTodoListApi("http://localhost:3000/api/todos", items);
+
+  return (
+    <div>
+      {state.isError && <div>Something went wrong ...</div>}
+      {state.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <TodoInputView></TodoInputView>
+          <TodoListView data={state.data}></TodoListView>
+        </div>
+      )}
+      <TodoItemCountView count={state.data.length}></TodoItemCountView>
+    </div>
+  );
+};
--- src/client/components/TodoListView.tsx
+++ src/client/components/TodoListView.tsx
@@ -1,104 +1,21 @@
-import React, { useEffect, useReducer, useState } from "react";
-import { TodoItemCountView } from "./TodoItemCountView";
+import React from "react";
 import { TodoItemView } from "./TodoItemView";
 
-const dataFetchReducer = (state: any, action: any) => {
-  switch (action.type) {
-    case "FETCH_INIT":
-      return { ...state, isLoading: true, isError: false };
-    case "FETCH_SUCCESS":
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload,
-      };
-    case "FETCH_FAILURE":
-      return { ...state, isLoading: false, isError: true };
-    default:
-      throw new Error();
-  }
-};
-
-const useTodoListApi = (url: string, initialData: any) => {
-  const [state, dispatch] = useReducer(dataFetchReducer, {
-    isLoading: false,
-    isError: false,
-    data: initialData,
-  });
-
-  useEffect(() => {
-    let didCancel = false;
-
-    const getApi = async (url: string) => {
-      const service = (
-        resolve: (value?: string) => void,
-        reject: (reason?: any) => void
-      ) => {
-        fetch(url)
-          .then((response) => response.json())
-          .then((json) => {
-            return resolve(json);
-          })
-          .catch((error) => {
-            return reject(error);
-          });
-      };
-      return new Promise(service);
-    };
-
-    (async () => {
-      dispatch({ type: "FETCH_INIT" });
-
-      try {
-        const result: any = await getApi(url);
-        const items = result.value.map((item: any) => ({
-          title: item.title.value,
-          status: item.status.value,
-          id: item.id,
-          completed: item.isCompleted,
-          dueDate: item.dueDate.value,
-        }));
-        dispatch({ type: "FETCH_SUCCESS", payload: items });
-      } catch (error) {
-        if (!didCancel) dispatch({ type: "FETCH_FAILURE" });
-      }
-    })();
-
-    return () => {
-      didCancel = true;
-    };
-  }, []);
-
-  return [state];
-};
-
-export const TodoListView: React.FC = () => {
-  const items = [
-    { title: "", status: "", id: 0, completed: false, dueDate: "" },
-  ];
-  const [state] = useTodoListApi("http://localhost:3000/api/todos", items);
-
+export const TodoListView: React.FC<{ data: any }> = (props: any) => {
   return (
     <div>
-      {state.isError && <div>Something went wrong ...</div>}
-      {state.isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <ul>
-          {state.data.map((item: any) => (
-            <TodoItemView
-              title={item.title}
-              status={item.status}
-              id={item.id}
-              completed={item.completed}
-              dueDate={item.dueDate}
-              key={item.id}
-            ></TodoItemView>
-          ))}
-        </ul>
-      )}
-      <TodoItemCountView count={state.data.length}></TodoItemCountView>
+      <ul>
+        {props.data.map((item: any) => (
+          <TodoItemView
+            title={item.title}
+            status={item.status}
+            id={item.id}
+            completed={item.completed}
+            dueDate={item.dueDate}
+            key={item.id}
+          ></TodoItemView>
+        ))}
+      </ul>
     </div>
   );
 };
