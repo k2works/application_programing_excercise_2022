7ac38ae07929df25d12d5ec6a71a85094be05aa1
k2works <kakimomokkuri@gmail.com>
2022-02-26 17:24:44 +0900

    refactor: 関数の抽出

--- src/client/app/Todo.tsx
+++ src/client/app/Todo.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import { TodoInputView } from "../components/TodoInputView";
 import { TodoListView } from "../components/TodoListView";
-import { Todo } from "./Todo";
 
-const App: React.FC = () => <Todo />;
-
-export default App;
+export const Todo: React.FC = () => (
+  <div>
+    <TodoInputView></TodoInputView>
+    <TodoListView></TodoListView>
+  </div>
+);
