1a43c1f1258b2e302054f62f21d7414e17d0237d
k2works <kakimomokkuri@gmail.com>
2022-03-05 16:33:36 +0900

    refactor: 名前の変更

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import App from "./app/main";
+import App from "./app/App";
 import rootReducer from "./reducers";
-
 import "./style.css";
+
 const dev = require("./Dev.js");
 dev.setUp();
 
