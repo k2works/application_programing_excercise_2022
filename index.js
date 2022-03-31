10b1f011cbed04d80be27a55f4064ddd7ddf7363
k2works <kakimomokkuri@gmail.com>
2022-03-31 17:06:50 +0900

    feat: データベース

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import "./src/style.css";
 import { App } from "./src/App.js";
+import { DB } from "./src/DB.js";
 import { setUp } from "./src/Dev.js";
-const app = new App();
-app.mount();
+const db = new DB();
+db.open(() => {
+  const app = new App(db);
+  app.mount();
+});
 setUp();
--- src/App.js
+++ src/App.js
@@ -3,41 +3,68 @@ import { TodoListModel } from "./model/TodoListModel";
 import { render } from "./view/html-util";
 import { TodoListView } from "./view/TodoListView";
 export class App {
-  constructor() {
+  constructor(db) {
+    this.db = db;
     this.todoListView = new TodoListView();
     this.todoListModel = new TodoListModel();
   }
 
   handleAdd(title) {
-    this.todoListModel.addTodo(new TodoItemModel({ title, completed: false }));
+    const todo = new TodoItemModel({ title, completed: false });
+    this.todoListModel.addTodo(todo);
+    this.db.addTodo(new TodoItemModel(todo), (todos) => {
+      this.todoListModel.items = todos;
+      this.render();
+    });
   }
 
   handleUpdate({ id, completed }) {
     this.todoListModel.updateTodo({ id, completed });
+    this.db.getTodo(id, (todo) => {
+      todo.completed = completed;
+      this.db.updateTodo(todo, (todos) => {
+        this.todoListModel.items = todos;
+        this.render();
+      });
+    });
   }
 
   handleDelete({ id }) {
     this.todoListModel.deleteTodo({ id });
+    this.db.deleteTodo(id, (todos) => {
+      this.todoListModel.items = todos;
+      this.render();
+    });
+  }
+
+  render() {
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+
+    const todoItems = this.todoListModel.getTodoItems();
+    const todoListElement = this.todoListView.createElement(todoItems, {
+      onUpdateTodo: ({ id, completed }) => {
+        this.handleUpdate({ id, completed });
+      },
+      onDeleteTodo: ({ id }) => {
+        this.handleDelete({ id });
+      },
+    });
+    render(todoListElement, containerElement);
+    todoItemCountElement.innerText = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
   }
 
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
-    const containerElement = document.querySelector("#js-todo-list");
-    const todoItemCountElement = document.querySelector("#js-todo-count");
+
+    this.db.getTodos((todos) => {
+      this.todoListModel.items = todos;
+      this.render();
+    });
 
     this.todoListModel.onChange(() => {
-      const todoItems = this.todoListModel.getTodoItems();
-      const todoListElement = this.todoListView.createElement(todoItems, {
-        onUpdateTodo: ({ id, completed }) => {
-          this.handleUpdate({ id, completed });
-        },
-        onDeleteTodo: ({ id }) => {
-          this.handleDelete({ id });
-        },
-      });
-      render(todoListElement, containerElement);
-      todoItemCountElement.innerText = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+      this.render();
     });
 
     formElement.addEventListener("submit", (event) => {
