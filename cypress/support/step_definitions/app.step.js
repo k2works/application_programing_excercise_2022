f2b8cc66aa39ce7cf45f30273150c1682e3f02e0
k2works <kakimomokkuri@gmail.com>
2022-04-02 13:29:39 +0900

    test: E2Eテスト

--- cypress/support/step_definitions/app.step.js
+++ cypress/support/step_definitions/app.step.js
@@ -14,6 +14,30 @@ Before(() => {
   cy.wait(1000);
 });
 
+Given(`{string} を追加する`, (value) => {
+  cy.get("#js-form-input").type(`${value}{enter}`);
+});
+
+Given(`{string} を更新する`, (value) => {
+  cy.get(".checkbox").click();
+});
+
+Given(`{string} を削除する`, (value) => {
+  cy.get(".delete").click();
+});
+
 Then(`{string} が表示される`, (value) => {
-  cy.get("#app-fizz-buzz-counter-counter").should("have.value", value);
+  cy.get("#js-todo-list > ul").should("contain", `${value}`);
+});
+
+Then(`{string} が更新される`, (value) => {
+  cy.get("s").should("contain", `${value}`);
+});
+
+Then(`{string} が削除される`, (value) => {
+  cy.get("#js-todo-list > ul").should("not.contain", `${value}`);
+});
+
+Then(`アイテム数が {string} 件になる`, (value) => {
+  cy.get("#js-todo-count").should("contain", value);
 });
