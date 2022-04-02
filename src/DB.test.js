95093da7a5c5dd4005089df4b645a6c28a15b844
k2works <kakimomokkuri@gmail.com>
2022-04-02 13:24:03 +0900

    fix: UnitTest

--- src/DB.test.js
+++ src/DB.test.js
@@ -3,55 +3,55 @@ let { DB } = require("./DB");
 let db = new DB();
 
 describe("IndexDBを利用するサンプルコード", () => {
-  test("Add1", () => {
-    db.open("test1").then(() => {
-      db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
+  test("Add1", async () => {
+    const db = new DB("test1");
+    await db.setup("test1");
+    db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
+      db.getTodos().then((todos) => {
+        expect(todos).toHaveLength(1);
+        expect(todos[0].title).toBe("todo1");
+        expect(todos[0].completed).toBe(false);
+      });
+    });
+  });
+
+  test("Add2", async () => {
+    const db = new DB("test2");
+    await db.setup();
+    db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
+      db.addTodo({ id: 2, title: "todo2", completed: false }).then(() => {
         db.getTodos().then((todos) => {
-          expect(todos).toHaveLength(1);
+          expect(todos).toHaveLength(2);
           expect(todos[0].title).toBe("todo1");
-          expect(todos[0].complelted).toBe(false);
+          expect(todos[0].completed).toBe(false);
         });
       });
     });
   });
 
-  test("Add2", () => {
-    db.open("test2").then(() => {
-      db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
-        db.addTodo({ id: 2, title: "todo2", completed: false }).then(() => {
+  test("Update", async () => {
+    const db = new DB("test3");
+    await db.setup();
+    db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
+      db.updateTodo({ id: 1, title: "todoUpdate", completed: true }).then(
+        () => {
           db.getTodos().then((todos) => {
-            expect(todos).toHaveLength(2);
-            expect(todos[0].title).toBe("todo1");
-            expect(todos[0].complelted).toBe(false);
+            expect(todos).toHaveLength(1);
+            expect(todos[0].title).toBe("todoUpdate");
+            expect(todos[0].completed).toBe(true);
           });
-        });
-      });
+        }
+      );
     });
   });
 
-  test("Update", () => {
-    db.open("test3").then(() => {
-      db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
-        db.updateTodo({ id: 1, title: "todoUpdate", completed: true }).then(
-          () => {
-            db.getTodos().then((todos) => {
-              expect(todos).toHaveLength(1);
-              expect(todos[0].title).toBe("todoUpdate");
-              expect(todos[0].complelted).toBe(true);
-            });
-          }
-        );
-      });
-    });
-  });
-
-  test("Delte", () => {
-    db.open("test4").then(() => {
-      db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
-        db.deleteTodo(1).then(() => {
-          db.getTodos().then((todos) => {
-            expect(todos).toHaveLength(0);
-          });
+  test("Delte", async () => {
+    const db = new DB("test4");
+    await db.setup();
+    db.addTodo({ id: 1, title: "todo1", completed: false }).then(() => {
+      db.deleteTodo(1).then(() => {
+        db.getTodos().then((todos) => {
+          expect(todos).toHaveLength(0);
         });
       });
     });
