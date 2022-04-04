ee8e1acffbce5c29cf200e8524d2828615072b54
k2works <kakimomokkuri@gmail.com>
2022-04-04 16:13:03 +0900

    refactor: 変数の抽出

--- src/component/TodoListComponent.jsx
+++ src/component/TodoListComponent.jsx
@@ -2,29 +2,23 @@ import React from "react";
 import { TodoItemComponent } from "./TodoItemComponent";
 
 export const TodoListComponent = () => {
+  const items = [
+    { title: "a", status: "完了", id: 0, completed: true, duDate: "" },
+    { title: "b", status: "未着手", id: 0, completed: false, duDate: "" },
+    { title: "a", status: "未着手", id: 0, completed: false, duDate: "" },
+  ];
+
   return (
     <ul>
-      <TodoItemComponent
-        title={"a"}
-        status={"完了"}
-        id={0}
-        completed={true}
-        dueDate={""}
-      ></TodoItemComponent>
-      <TodoItemComponent
-        title={"b"}
-        status={"未着手"}
-        id={0}
-        completed={false}
-        dueDate={""}
-      ></TodoItemComponent>
-      <TodoItemComponent
-        title={"c"}
-        status={"未着手"}
-        id={0}
-        completed={false}
-        dueDate={""}
-      ></TodoItemComponent>
+      {items.map((item) => (
+        <TodoItemComponent
+          title={item.title}
+          status={item.status}
+          id={item.id}
+          completed={item.completed}
+          dueDate={item.duDate}
+        ></TodoItemComponent>
+      ))}
     </ul>
   );
 };
