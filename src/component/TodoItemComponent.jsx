26eeac56503c84000c4361e7f28f591cd83cf7c7
k2works <kakimomokkuri@gmail.com>
2022-04-04 16:03:51 +0900

    refactor: 関数の移動

--- src/component/TodoItemComponent.jsx
+++ src/component/TodoItemComponent.jsx
@@ -1,37 +1,13 @@
 import React from "react";
 
-export const TodoItemComponent = () => {
-  const li1 = (
+export const TodoItemComponent = (props) => {
+  return (
     <li className=" status not-started">
       <input type="checkbox" placeholder="check" className="checkbox" />
-      a By
+      {props.title} By
       <input className="due" type="date" value="" placeholder="check" />
-      未着手
+      {props.status}
       <button className="delete">x</button>
     </li>
   );
-
-  const li2 = (
-    <li className=" status not-started">
-      <input type="checkbox" className="checkbox" placeholder="input" />
-      b By
-      <input className="due" type="date" placeholder="input" />
-      未着手
-      <button className="delete">x</button>
-    </li>
-  );
-
-  const li3 = (
-    <li className=" status not-started">
-      <input type="checkbox" className="checkbox" placeholder="input" />
-      c By
-      <input className="due" type="date" placeholder="input" />
-      未着手
-      <button className="delete">x</button>
-    </li>
-  );
-
-  const list = [li1, li2, li3];
-
-  return <ul>{list.map((li, index) => li)}</ul>;
 };
--- src/component/TodoListComponent.jsx
+++ src/component/TodoListComponent.jsx
@@ -2,5 +2,29 @@ import React from "react";
 import { TodoItemComponent } from "./TodoItemComponent";
 
 export const TodoListComponent = () => {
-  return <TodoItemComponent />;
+  return (
+    <ul>
+      <TodoItemComponent
+        title={"a"}
+        status={"未着手"}
+        id={0}
+        completed={false}
+        dueDate={""}
+      ></TodoItemComponent>
+      <TodoItemComponent
+        title={"b"}
+        status={"未着手"}
+        id={0}
+        completed={false}
+        dueDate={""}
+      ></TodoItemComponent>
+      <TodoItemComponent
+        title={"c"}
+        status={"未着手"}
+        id={0}
+        completed={false}
+        dueDate={""}
+      ></TodoItemComponent>
+    </ul>
+  );
 };
