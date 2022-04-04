b61b761645030c1407951a063aef0cd235f77f7e
k2works <kakimomokkuri@gmail.com>
2022-04-04 16:01:08 +0900

    refactor: 関数の抽出

--- src/component/TodoItemComponent.jsx
+++ src/component/TodoItemComponent.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 
 export const TodoItemComponent = () => {
-  return (
-    <ul>
-      <li className=" status not-started">
-        <input type="checkbox" placeholder="check" className="checkbox" />
-        a
-        <input className="due" type="date" value="" placeholder="check" />
-        未着手
-        <button className="delete">x</button>
-      </li>
-      <li className=" status not-started">
-        <input type="checkbox" className="checkbox" placeholder="input" />
-        b
-        <input className="due" type="date" placeholder="input" />
-        未着手
-        <button className="delete">x</button>
-      </li>
-      <li className=" status not-started">
-        <input type="checkbox" className="checkbox" placeholder="input" />
-        c
-        <input className="due" type="date" placeholder="input" />
-        未着手
-        <button className="delete">x</button>
-      </li>
-    </ul>
+  const li1 = (
+    <li className=" status not-started">
+      <input type="checkbox" placeholder="check" className="checkbox" />
+      a By
+      <input className="due" type="date" value="" placeholder="check" />
+      未着手
+      <button className="delete">x</button>
+    </li>
   );
+
+  const li2 = (
+    <li className=" status not-started">
+      <input type="checkbox" className="checkbox" placeholder="input" />
+      b By
+      <input className="due" type="date" placeholder="input" />
+      未着手
+      <button className="delete">x</button>
+    </li>
+  );
+
+  const li3 = (
+    <li className=" status not-started">
+      <input type="checkbox" className="checkbox" placeholder="input" />
+      c By
+      <input className="due" type="date" placeholder="input" />
+      未着手
+      <button className="delete">x</button>
+    </li>
+  );
+
+  const list = [li1, li2, li3];
+
+  return <ul>{list.map((li, index) => li)}</ul>;
 };
