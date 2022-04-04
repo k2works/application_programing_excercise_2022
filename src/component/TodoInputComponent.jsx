322101b2b39ddf23a053255472afc15c6001ae5b
k2works <kakimomokkuri@gmail.com>
2022-04-04 16:09:50 +0900

    refactor: アルゴリズムの変更

--- src/component/TodoInputComponent.jsx
+++ src/component/TodoInputComponent.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export const TodoInputComponent = () => {
   return (
-    <input
-      id="js-form-input"
-      type="text"
-      placeholder="What need to be done?"
-      autocomplete="off"
-    ></input>
+    <form id="js-form">
+      <input
+        id="js-form-input"
+        type="text"
+        placeholder="What need to be done?"
+        autoComplete="off"
+      />
+    </form>
   );
 };
