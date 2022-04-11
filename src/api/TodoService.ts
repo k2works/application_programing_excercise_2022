9e82336ea96310b427f96df3999736325867169e
k2works <kakimomokkuri@gmail.com>
2022-04-11 17:20:17 +0900

    refactor: アルゴリズムの変更

--- src/api/TodoService.ts
+++ src/api/TodoService.ts
@@ -18,83 +18,52 @@ export type Params = {
 
 export class TodoService {
   execute(type: number, params: Params) {
-    return new Promise((resolve: any, reject: any) => {
+    return new Promise(async (resolve: any, reject: any) => {
       if (type === Type.CREATE) {
         const todo = new Todo();
         todo.title = params.title ? params.title : "";
         todo.completed = false;
         todo.createdAt = new Date();
         todo.status = "未着手";
-        todo
-          .save()
-          .then((result: Todo) => {
-            resolve(result);
-          })
-          .catch((err: any) => {
-            reject(err);
-          });
+        const result = await todo.save();
+        resolve(result);
       } else if (type === Type.READ) {
-        Todo.find()
-          .then((result: Todo[]) => {
-            resolve(result);
-          })
-          .catch((err: any) => {
-            reject(err);
-          });
+        const result = await Todo.find();
+        resolve(result);
       } else if (type === Type.UPDATE) {
         const id = params.id;
         if (id) {
-          Todo.findOneBy({ id }).then((result: Todo | null) => {
-            if (result) {
-              const dueDate = params.dueDate;
-              if (dueDate) {
-                const completedAt = params.completed ? null : new Date();
-                const status = params.completed ? "完了" : "着手";
-                result.completed = params.completed ? true : false;
-                result.dueDate = dueDate;
-                result.completedAt = completedAt;
-                result.status = status;
-                result
-                  .save()
-                  .then((result: any) => {
-                    resolve(result);
-                  })
-                  .catch((err: any) => {
-                    reject(err);
-                  });
-              } else {
-                const completedAt = params.completed ? null : new Date();
-                const status = params.completed ? "完了" : "着手";
-                result.completed = params.completed ? true : false;
-                result.completedAt = completedAt;
-                result.status = status;
-                result
-                  .save()
-                  .then((result: any) => {
-                    resolve(result);
-                  })
-                  .catch((err: any) => {
-                    reject(err);
-                  });
-              }
+          const result = await Todo.findOneBy({ id });
+          if (result) {
+            const dueDate = params.dueDate;
+            if (dueDate) {
+              const completedAt = params.completed ? null : new Date();
+              const status = params.completed ? "完了" : "着手";
+              result.completed = params.completed ? true : false;
+              result.dueDate = dueDate;
+              result.completedAt = completedAt;
+              result.status = status;
+              await result.save();
+              resolve();
+            } else {
+              const completedAt = params.completed ? null : new Date();
+              const status = params.completed ? "完了" : "着手";
+              result.completed = params.completed ? true : false;
+              result.completedAt = completedAt;
+              result.status = status;
+              await result.save();
+              resolve();
             }
-          });
+          }
         }
       } else if (type === Type.DELETE) {
         const id = params.id;
         if (id) {
-          Todo.findOneBy({ id }).then((result: Todo | null) => {
-            if (result) {
-              result
-                .remove()
-                .then((result: any) => {
-                  resolve(result);
-                })
-                .catch((err: any) => {
-                  reject(err);
-                });
-            }
-          });
+          const resutl = await Todo.findOneBy({ id });
+          if (resutl) {
+            await resutl.remove();
+            resolve();
+          }
         } else {
           reject(new Error("Invalid type"));
         }
