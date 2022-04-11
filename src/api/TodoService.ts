c197bf825d994ace4a72c2117f8d1f63c1c22a3e
k2works <kakimomokkuri@gmail.com>
2022-04-11 17:24:40 +0900

    refactor: アルゴリズムの変更

--- src/api/TodoService.ts
+++ src/api/TodoService.ts
@@ -17,57 +17,50 @@ export type Params = {
 };
 
 export class TodoService {
-  execute(type: number, params: Params) {
-    return new Promise(async (resolve: any, reject: any) => {
-      if (type === Type.CREATE) {
-        const todo = new Todo();
-        todo.title = params.title ? params.title : "";
-        todo.completed = false;
-        todo.createdAt = new Date();
-        todo.status = "未着手";
-        const result = await todo.save();
-        resolve(result);
-      } else if (type === Type.READ) {
-        const result = await Todo.find();
-        resolve(result);
-      } else if (type === Type.UPDATE) {
-        const id = params.id;
-        if (id) {
-          const result = await Todo.findOneBy({ id });
-          if (result) {
-            const dueDate = params.dueDate;
-            if (dueDate) {
-              const completedAt = params.completed ? null : new Date();
-              const status = params.completed ? "完了" : "着手";
-              result.completed = params.completed ? true : false;
-              result.dueDate = dueDate;
-              result.completedAt = completedAt;
-              result.status = status;
-              await result.save();
-              resolve();
-            } else {
-              const completedAt = params.completed ? null : new Date();
-              const status = params.completed ? "完了" : "着手";
-              result.completed = params.completed ? true : false;
-              result.completedAt = completedAt;
-              result.status = status;
-              await result.save();
-              resolve();
-            }
+  async execute(type: number, params: Params) {
+    if (type === Type.CREATE) {
+      const todo = new Todo();
+      todo.title = params.title ? params.title : "";
+      todo.completed = false;
+      todo.createdAt = new Date();
+      todo.status = "未着手";
+      await todo.save();
+    } else if (type === Type.READ) {
+      return await Todo.find();
+    } else if (type === Type.UPDATE) {
+      const id = params.id;
+      if (id) {
+        const result = await Todo.findOneBy({ id });
+        if (result) {
+          const dueDate = params.dueDate;
+          if (dueDate) {
+            const completedAt = params.completed ? null : new Date();
+            const status = params.completed ? "完了" : "着手";
+            result.completed = params.completed ? true : false;
+            result.dueDate = dueDate;
+            result.completedAt = completedAt;
+            result.status = status;
+            await result.save();
+          } else {
+            const completedAt = params.completed ? null : new Date();
+            const status = params.completed ? "完了" : "着手";
+            result.completed = params.completed ? true : false;
+            result.completedAt = completedAt;
+            result.status = status;
+            await result.save();
           }
-        }
-      } else if (type === Type.DELETE) {
-        const id = params.id;
-        if (id) {
-          const resutl = await Todo.findOneBy({ id });
-          if (resutl) {
-            await resutl.remove();
-            resolve();
-          }
-        } else {
-          reject(new Error("Invalid type"));
         }
       }
-    });
+    } else if (type === Type.DELETE) {
+      const id = params.id;
+      if (id) {
+        const resutl = await Todo.findOneBy({ id });
+        if (resutl) {
+          return await resutl.remove();
+        }
+      }
+    } else {
+      new Error("Invalid type");
+    }
   }
 }
