ae6f598f9aa7be0671f36a9376917145312d7e65
k2works <kakimomokkuri@gmail.com>
2022-04-12 15:38:33 +0900

    refactor: クラスの抽出

--- src/api/Todo.test.ts
+++ src/api/Todo.test.ts
@@ -1,6 +1,12 @@
 import { Todo } from "./Todo";
 
 describe("Todo", () => {
+  test("内容が同じであれば同じTodo", () => {
+    const todo1 = new Todo("タイトル");
+    const todo2 = new Todo("タイトル");
+    expect(todo1.equals(todo2)).toBe(true);
+  });
+
   test("やることを生成する", () => {
     const todo = new Todo("タイトル");
     expect(todo.Title).toBe("タイトル");
--- src/api/Todo.ts
+++ src/api/Todo.ts
@@ -2,11 +2,11 @@
  * やること
  */
  export class Todo {
-   private title: string;
+   private title: Title;
    private completed: boolean;
 
    get Title(): string {
-     return this.title;
+     return this.title.Value;
    }
 
    get Completed(): boolean {
@@ -14,11 +14,36 @@
    }
 
    constructor(title: string) {
-     this.title = title;
+     this.title = new Title(title);
      this.completed = false;
    }
 
    public complete(): void {
      this.completed = true;
+   }
+
+   public equals(other: Todo): boolean {
+     return (
+       this.title.equals(other.title) && this.completed === other.completed
+     );
+   }
+ }
+
+ /**
+  * タイトル
+  */
+ class Title {
+   private value: string;
+
+   get Value(): string {
+     return this.value;
+   }
+
+   constructor(value: string) {
+     this.value = value;
+   }
+
+   public equals(other: Title): boolean {
+     return this.Value === other.Value;
    }
  }
