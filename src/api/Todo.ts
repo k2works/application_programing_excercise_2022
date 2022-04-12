10c2ccf476bdd173b4e7742a5e0fc63ae30f8de8
k2works <kakimomokkuri@gmail.com>
2022-04-12 17:12:12 +0900

    refactor: メソッドの移動

--- src/api/Todo.ts
+++ src/api/Todo.ts
@@ -47,12 +47,7 @@ export class Todo {
   }
 
   public overDue(): boolean {
-    const due = this.dueDate.Value;
-    const today = new Date();
-    if (due === null) {
-      return false;
-    }
-    return today.getTime() > due.getTime();
+    return this.dueDate.overDue();
   }
 
   public setDueDate(due: DueDate): Todo {
@@ -153,5 +148,14 @@ export class DueDate {
 
   public equals(dueDate: DueDate): boolean {
     return this.value?.getTime() === dueDate.Value?.getTime();
+  }
+
+  public overDue(): boolean {
+    const due = this.Value;
+    const today = new Date();
+    if (due === null) {
+      return false;
+    }
+    return today.getTime() > due.getTime();
   }
 }
