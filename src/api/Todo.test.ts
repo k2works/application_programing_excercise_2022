a13c1cb1c9ff8470d7991fa09e7fb00dabbe647b
k2works <kakimomokkuri@gmail.com>
2022-04-12 16:53:49 +0900

    test: ドメインモデル

--- src/api/Todo.test.ts
+++ src/api/Todo.test.ts
@@ -1,12 +1,6 @@
 import { Todo } from "./Todo";
 
 describe("Todo", () => {
-  test("内容が同じであれば同じTodo", () => {
-    const todo1 = new Todo("タイトル");
-    const todo2 = new Todo("タイトル");
-    expect(todo1.equals(todo2)).toBe(true);
-  });
-
   test("やることを生成する", () => {
     const todo = new Todo("タイトル");
     expect(todo.Title).toBe("タイトル");
@@ -17,5 +11,37 @@ describe("Todo", () => {
     const todo = new Todo("タイトル");
     todo.complete();
     expect(todo.Completed).toBe(true);
+  });
+
+  //TODO: エンティティとして実装する
+  test.skip("内容が同じであれば同じTodo", () => {
+    const todo1 = new Todo("タイトル");
+    const todo2 = new Todo("タイトル");
+    expect(todo1.equals(todo2)).toBe(true);
+  });
+
+  test("やることを生成したら開始日を設定する", () => {
+    const todo = new Todo("タイトル");
+    expect(todo.CreatedAt).toBeDefined();
+  });
+
+  test("やることを更新したら完了日を設定する", () => {
+    const todo = new Todo("タイトル");
+    todo.complete();
+    expect(todo.CompletedAt).toBeDefined();
+  });
+
+  test("やることに期限を設定する", () => {
+    const todo = new Todo("タイトル");
+    todo.DueDate = new Date();
+    expect(todo.DueDate).toBeDefined();
+  });
+
+  test("やることの期限を確認する", () => {
+    const todo = new Todo("タイトル");
+    const due = new Date();
+    due.setDate(due.getDate() - 1);
+    todo.DueDate = due;
+    expect(todo.overDue()).toBe(true);
   });
 });
--- src/api/Todo.ts
+++ src/api/Todo.ts
@@ -1,49 +1,79 @@
+export class Todo {
+  private title: Title;
+  private completed: boolean;
+  private createdAt: Date;
+  private completedAt: Date | null;
+  private dueDate: Date | null;
+
+  get Title(): string {
+    return this.title.Value;
+  }
+
+  get Completed(): boolean {
+    return this.completed;
+  }
+
+  get CreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  get CompletedAt(): Date | null {
+    return this.completedAt;
+  }
+
+  get DueDate(): Date | null {
+    return this.dueDate;
+  }
+
+  set DueDate(date: Date | null) {
+    this.dueDate = date;
+  }
+
+  constructor(title: string) {
+    this.title = new Title(title);
+    this.completed = false;
+    this.createdAt = new Date();
+    this.completedAt = null;
+    this.dueDate = null;
+  }
+
+  public complete(): void {
+    this.completed = true;
+  }
+
+  public overDue(): boolean {
+    if (this.dueDate === null) {
+      return false;
+    }
+    return this.createdAt.getTime() > this.dueDate.getTime();
+  }
+
+  public equals(other: Todo): boolean {
+    return (
+      this.title.equals(other.title) &&
+      this.completed === other.completed &&
+      this.createdAt === other.createdAt &&
+      this.completedAt === other.completedAt &&
+      this.dueDate === other.dueDate
+    );
+  }
+}
+
 /**
- * やること
+ * タイトル
  */
- export class Todo {
-   private title: Title;
-   private completed: boolean;
+class Title {
+  private value: string;
 
-   get Title(): string {
-     return this.title.Value;
-   }
+  get Value(): string {
+    return this.value;
+  }
 
-   get Completed(): boolean {
-     return this.completed;
-   }
+  constructor(value: string) {
+    this.value = value;
+  }
 
-   constructor(title: string) {
-     this.title = new Title(title);
-     this.completed = false;
-   }
-
-   public complete(): void {
-     this.completed = true;
-   }
-
-   public equals(other: Todo): boolean {
-     return (
-       this.title.equals(other.title) && this.completed === other.completed
-     );
-   }
- }
-
- /**
-  * タイトル
-  */
- class Title {
-   private value: string;
-
-   get Value(): string {
-     return this.value;
-   }
-
-   constructor(value: string) {
-     this.value = value;
-   }
-
-   public equals(other: Title): boolean {
-     return this.Value === other.Value;
-   }
- }
+  public equals(other: Title): boolean {
+    return this.Value === other.Value;
+  }
+}
