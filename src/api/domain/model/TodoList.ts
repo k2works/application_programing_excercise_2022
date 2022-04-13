c7b0c6fa45329fd6be53912ae04c61e37bed2305
k2works <kakimomokkuri@gmail.com>
2022-04-13 12:40:59 +0900

    test: ファーストクラスコレクション

---
+++ src/api/domain/model/TodoList.ts
@@ -0,0 +1,20 @@
+import { Todo } from "./Todo";
+
+/**
+ * やることリスト
+ */
+export class TodoList {
+  private value: Todo[];
+
+  get Value() {
+    return this.value;
+  }
+
+  constructor(value: Todo[] = []) {
+    this.value = value;
+  }
+
+  add(todo: Todo): TodoList {
+    return new TodoList(this.value.map((t) => t).concat(todo));
+  }
+}
