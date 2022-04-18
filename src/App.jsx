605a26ebffff89aa87093255a76b1f9a3320a6d8
k2works <kakimomokkuri@gmail.com>
2022-04-18 18:28:54 +0900

    STEP 1-1 ヘッダーをマークアップする

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import * as React from "react";
-import { Content } from "./component/Content";
 
 const App = () => (
   <div>
-    <Content name="Content" />
+    <header className="header">
+      <div className="header-container">
+        <a href="index.html">
+          <img src="logo.svg" alt="Boards" width={135} height={25} />
+        </a>
+      </div>
+
+      <button className="navbtn">
+        <i className="fas fa-bars"></i>
+        <span className="sr-only">MENU</span>
+      </button>
+    </header>
   </div>
 );
 
