07b411a9cbe2dcc58b2997bf0cec4ea9b4e1d203
k2works <kakimomokkuri@gmail.com>
2022-04-19 18:58:22 +0900

    STEP 6-1 コンテンツページを作成する

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Logo from "../img/logo.svg";
 
 export const Content = (props) => {
   return (
     <div>
-      <h2>{props.name}</h2>
+      <header className="header">
+        <div className="header-container w-container">
+          <a href="/">
+            <img src={Logo} alt="logo" width={135} height={25} />
+          </a>
+
+          <button className="navbtn">
+            <i className="fas fa-bars"></i>
+            <span className="sr-only">MENU</span>
+          </button>
+        </div>
+      </header>
     </div>
   );
 };
