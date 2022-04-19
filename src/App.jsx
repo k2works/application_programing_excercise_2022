9a62c4c90124bd6846d05e43ac19906e454a920d
k2works <kakimomokkuri@gmail.com>
2022-04-19 17:26:04 +0900

    STEP 5-1 フッターをマークアップする

--- src/App.jsx
+++ src/App.jsx
@@ -142,6 +142,52 @@ const App = () => (
         </div>
       </div>
     </section>
+
+    <footer className="footer">
+      <div className="footer-container w-container">
+        <a href="index.html">
+          <img src={Logo} alt="Boards" width={135} height={26} />
+        </a>
+      </div>
+      <ul className="footer-sns">
+        <li>
+          <a href="#">
+            <i className="fab fa-twitter"></i>
+            <span className="sr-only">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fab fa-facebook-f"></i>
+            <span className="sr-only">Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fab fa-instagram"></i>
+            <span className="sr-only">Instagram</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="footer-menu">
+        <li>
+          <a href="#">会社外由生</a>
+        </li>
+        <li>
+          <a href="#">特定商取引</a>
+        </li>
+        <li>
+          <a href="#">個人情報の取り扱い</a>
+        </li>
+        <li>
+          <a href="content.html">サービス案内</a>
+        </li>
+        <li>
+          <a href="#">お問い合わせ</a>
+        </li>
+      </ul>
+      <div className="footer-copy">@ Boards Production.</div>
+    </footer>
   </div>
 );
 
