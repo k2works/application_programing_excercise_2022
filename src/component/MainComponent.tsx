d720fbdc2712a0e168a697b75df1c5fa27d044f3
k2works <kakimomokkuri@gmail.com>
2022-04-20 17:48:16 +0900

    STEP 1-1 ヘッダーをマークアップする

--- src/component/MainComponent.tsx
+++ src/component/MainComponent.tsx
@@ -1,10 +1,24 @@
 import * as React from "react";
 import "../style.css";
+import Log from "../img/logo.svg";
 
 export const Main: React.FC<{}> = () => {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <header className="header">
+        <div className="header-container">
+          <div className="site">
+            <a href="/">
+              <img src={Log} alt="" width={135} height={26} />
+            </a>
+          </div>
+
+          <button className="navbtn">
+            <i className="fas fa-bars"></i>
+            <span className="sr-only">MENU</span>
+          </button>
+        </div>
+      </header>
     </div>
   );
 };
