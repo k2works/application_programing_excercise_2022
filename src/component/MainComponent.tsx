cf54ba869289eb6cf810542f12adeb6acd161a98
k2works <kakimomokkuri@gmail.com>
2022-04-21 16:45:26 +0900

    STEP 3-1 嘉造とテキストをマークアップする

--- src/component/MainComponent.tsx
+++ src/component/MainComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "../style.css";
 import Log from "../img/logo.svg";
+import Tool from "../img/tool.jpg";
 
 export const Main: React.FC<{}> = () => {
   return (
@@ -27,6 +28,21 @@ export const Main: React.FC<{}> = () => {
           <a href="#" className="btn">
             無料で始める
           </a>
+        </div>
+      </section>
+
+      <section className="imgtext">
+        <div className="imgtext-container w-container">
+          <div className="text">
+            <h2>日常のツールたち</h2>
+            <p>Convenient</p>
+            <p>
+              どこにでもある、誰でも使ったことがある、普段は存在を意識しないけどないと困るツールたち。日常をちょっと楽にしてくれます。
+            </p>
+            <figure className="img">
+              <img src={Tool} alt="" width={1600} height={1260} />
+            </figure>
+          </div>
         </div>
       </section>
     </div>
