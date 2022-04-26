d76be52e5ee2e1339245b1cf51d0b5cd58d5f77e
k2works <kakimomokkuri@gmail.com>
2022-04-26 16:29:31 +0900

    refactor: コンポーネントの抽出

--- src/component/ContentComponent.tsx
+++ src/component/ContentComponent.tsx
@@ -1,44 +1,13 @@
 import React from "react";
 import "../style.css";
-import Log from "../img/logo.svg";
 import Service from "../img/service.jpg";
+import { Header } from "./HeaderComponent";
+import { Footer } from "./FooterComponent";
 
 export const Content: React.FC<{}> = () => {
-  const openMenu = () => {
-    document.querySelector("html")?.classList.toggle("open");
-  };
-
   return (
     <div>
-      <header className="header">
-        <div className="header-container w-container">
-          <div className="site">
-            <a href="/">
-              <img src={Log} alt="" width={135} height={26} />
-            </a>
-          </div>
-
-          <button className="navbtn" onClick={openMenu}>
-            <i className="fas fa-bars"></i>
-            <i className="fas fa-times"></i>
-            <span className="sr-only">MENU</span>
-          </button>
-
-          <nav className="nav">
-            <ul>
-              <li>
-                <a href="/">ホーム</a>
-              </li>
-              <li>
-                <a href="/content">サービス案内</a>
-              </li>
-              <li>
-                <a href="#">お問い合わせ</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+      <Header />
 
       <article className="entry">
         <figure className="entry-img">
@@ -113,56 +82,7 @@ export const Content: React.FC<{}> = () => {
         </div>
       </section>
 
-      <footer className="footer">
-        <div className="footer-container w-container">
-          <div className="footer-site">
-            <a href="/">
-              <img src={Log} alt="Boards" width={135} height={26} />
-            </a>
-          </div>
-
-          <ul className="footer-sns">
-            <li>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-                <span className="sr-only">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-facebook-f"></i>
-                <span className="sr-only">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fab fa-instagram"></i>
-                <span className="sr-only">Instagram</span>
-              </a>
-            </li>
-          </ul>
-
-          <ul className="footer-menu">
-            <li>
-              <a href="#">会社概要</a>
-            </li>
-            <li>
-              <a href="#">特定商取引法</a>
-            </li>
-            <li>
-              <a href="#">個人情報の取り扱い</a>
-            </li>
-            <li>
-              <a href="content">サービス案内</a>
-            </li>
-            <li>
-              <a href="#">お問い合わせ</a>
-            </li>
-          </ul>
-
-          <div className="footer-copy">@ Boards Production.</div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
