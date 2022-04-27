f27e750dbb2081b6c9bebdd5eb787a06d29136aa
k2works <kakimomokkuri@gmail.com>
2022-04-27 14:49:54 +0900

    feat: 仮実装

--- src/main/typescript/components/main-component.tsx
+++ src/main/typescript/components/main-component.tsx
@@ -7,16 +7,59 @@ import { SubComponent } from "./sub-component";
 export const MainComponent: React.FC<{}> = () => {
   return (
     <div>
-      <h1>Hello React!</h1>
-      <SubComponent name="My Counter for Babel" />
+      <h1>タスク管理アプリケーション</h1>
 
-      <img src={Image} alt="image" />
+      <div className="task_form">
+        <h2>タスクの登録</h2>
 
-      <div className="my-grid">
-        <header className="my-grid-item">ヘッダー</header>
-        <aside className="my-grid-item">サイドバー</aside>
-        <main className="my-grid-item">メインコンテンツ</main>
-        <footer className="my-grid-item">フッター</footer>
+        <form action="/add">
+          <label>タスク</label>
+          <input name="task" type="text" />
+          <label>期限</label>
+          <input name="deadline" type="date" />
+          <input value="登録" type="submit" />
+        </form>
+      </div>
+
+      <div className="tasklist">
+        <h2>現在のタスク一覧</h2>
+        <table>
+          <thead>
+            <tr>
+              <th className="hidden">ID</th>
+              <th>タスク</th>
+              <th>期限</th>
+              <th>状態</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className="hidden">1</td>
+              <td>Java本の原稿を入港する</td>
+              <td>2021-09-30</td>
+              <td>未完了</td>
+              <input type="button" value="更新" />
+              <input type="button" value="削除" />
+            </tr>
+            <tr>
+              <td className="hidden">2</td>
+              <td>猫の予防接種を受ける</td>
+              <td>2021-10-10</td>
+              <td>未完了</td>
+              <input type="button" value="更新" />
+              <input type="button" value="削除" />
+            </tr>
+            <tr>
+              <td className="hidden">3</td>
+              <td>卵と牛乳と豆腐を買う</td>
+              <td>2021-10-15</td>
+              <td>未完了</td>
+              <input type="button" value="更新" />
+              <input type="button" value="削除" />
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
