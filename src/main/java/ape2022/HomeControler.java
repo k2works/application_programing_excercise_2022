1abf569958c186cc67d17e4d6d0a25649e12cf26
k2works <kakimomokkuri@gmail.com>
2022-04-29 17:03:54 +0900

    Webアプリケーションにデータベースを組み込む

--- src/main/java/ape2022/HomeControler.java
+++ src/main/java/ape2022/HomeControler.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,13 @@ public class HomeControler {
 
   private List<TaskItem> taskItems = new ArrayList<>();
 
+  private final TaskListDao dao;
+
+  @Autowired
+  HomeControler(TaskListDao dao) {
+    this.dao = dao;
+  }
+
   @RequestMapping(value = "/hello")
   String hello(Model model) {
     model.addAttribute("time", LocalDateTime.now());
@@ -26,6 +34,7 @@ public class HomeControler {
 
   @GetMapping("/list")
   String listItems(Model model) {
+    List<TaskItem> taskItems = dao.findAll();
     model.addAttribute("taskList", taskItems);
     return "home";
   }
@@ -34,8 +43,22 @@ public class HomeControler {
   String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
     String id = UUID.randomUUID().toString().substring(0, 8);
     TaskItem item = new TaskItem(id, task, deadline, false);
-    taskItems.add(item);
+    dao.add(item);
 
+    return "redirect:/list";
+  }
+
+  @GetMapping("/delete")
+  String deleteItem(@RequestParam("id") String id) {
+    dao.delete(id);
+    return "redirect:/list";
+  }
+
+  @GetMapping("/update")
+  String updateItem(@RequestParam("id") String id, @RequestParam("task") String task,
+      @RequestParam("deadline") String deadline, @RequestParam("done") boolean done) {
+    TaskItem item = new TaskItem(id, task, deadline, done);
+    dao.update(item);
     return "redirect:/list";
   }
 }
