d6ec9b03eb19bae4f006d1775dbbf917f8c1093f
k2works <kakimomokkuri@gmail.com>
2022-04-29 13:28:09 +0900

    ユーザーインタフェースの作成にテンプレートエンジンを活用する

--- src/main/java/ape2022/HomeControler.java
+++ src/main/java/ape2022/HomeControler.java
@@ -1,26 +1,41 @@
 package ape2022;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeControler {
+  record TaskItem(String id, String task, String deadline, boolean done) {
+  }
+
+  private List<TaskItem> taskItems = new ArrayList<>();
+
   @RequestMapping(value = "/hello")
-  @ResponseBody
-  String hello() {
-    return """
-        <html>
-          <head>
-            <title>Hello</title>
-          </head>
-          <body>
-            <h1>Hello</h1>
-            It works!<br>
-            現在時刻は%sです。
-          </body>
-        """.formatted(LocalDateTime.now());
+  String hello(Model model) {
+    model.addAttribute("time", LocalDateTime.now());
+    return "hello";
+  }
+
+  @GetMapping("/list")
+  String listItems(Model model) {
+    model.addAttribute("taskList", taskItems);
+    return "home";
+  }
+
+  @GetMapping("/add")
+  String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
+    String id = UUID.randomUUID().toString().substring(0, 8);
+    TaskItem item = new TaskItem(id, task, deadline, false);
+    taskItems.add(item);
+
+    return "redirect:/list";
   }
 }
