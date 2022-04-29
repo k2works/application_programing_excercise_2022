a4b94b926405429c9e519063a18bd66f5a4b7641
k2works <kakimomokkuri@gmail.com>
2022-04-29 11:35:18 +0900

    ユーザーインタフェースの作成にテンプレートエンジンを活用する

---
+++ src/main/java/ape2022/HomeControler.java
@@ -0,0 +1,26 @@
+package ape2022;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeControler {
+  @RequestMapping(value = "/hello")
+  @ResponseBody
+  String hello() {
+    return """
+        <html>
+          <head>
+            <title>Hello</title>
+          </head>
+          <body>
+            <h1>Hello</h1>
+            It works!<br>
+            現在時刻は%sです。
+          </body>
+        """.formatted(LocalDateTime.now());
+  }
+}
