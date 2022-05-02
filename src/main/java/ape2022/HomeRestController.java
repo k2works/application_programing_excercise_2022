9fea15fb3540d45e75e1038cac4ceb9ec02f2111
k2works <kakimomokkuri@gmail.com>
2022-05-02 17:45:44 +0900

    feat(WIP): タスク一覧を表示する

--- src/main/java/ape2022/HomeRestController.java
+++ src/main/java/ape2022/HomeRestController.java
@@ -8,17 +8,24 @@ import java.util.UUID;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ape2022.HomeControler.TaskItem;
+
 @RestController
 public class HomeRestController {
-  record TaskItem(String id, String task, String deadline, boolean done) {
-  }
-
   private List<TaskItem> taskItems = new ArrayList<>();
+
+  private final TaskListDao dao;
+
+  @Autowired
+  HomeRestController(TaskListDao dao) {
+    this.dao = dao;
+  }
 
   @GetMapping("/restadd")
   String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
@@ -30,9 +37,9 @@ public class HomeRestController {
   }
 
   @GetMapping("/restlist")
-  String listitems() {
-    String result = taskItems.stream().map(TaskItem::toString).collect(Collectors.joining(","));
-    return result;
+  List<TaskItem> listitems() {
+    List<TaskItem> taskItems = dao.findAll();
+    return taskItems;
   }
 
   @RequestMapping(value = "/resthello")
