98dfb2c6f2d9f4e4a15f055d3767efa27934eee4
k2works <kakimomokkuri@gmail.com>
2022-05-11 17:58:33 +0900

    chore: SpringBootセットアップ

---
+++ src/test/java/k2works/mrs/MrsApplicationTests.java
@@ -0,0 +1,13 @@
+package k2works.mrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
