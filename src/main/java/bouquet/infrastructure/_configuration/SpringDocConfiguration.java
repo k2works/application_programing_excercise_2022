4e94ea09a50ec096c5482288d5242d0afd9b4fb1
k2works <kakimomokkuri@gmail.com>
2022-08-22 16:44:32 +0900

    chore: springdoc-openapi-uiセットアップ

--- src/main/java/bouquet/infrastructure/_configuration/SpringDocConfiguration.java
+++ src/main/java/bouquet/infrastructure/_configuration/SpringDocConfiguration.java
@@ -1,4 +1,4 @@
-package bouquet.infrastructure.datasource._configuration;
+package bouquet.infrastructure._configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
