8493eec8fc83901ac0751c34e3c3d8a58eb633be
k2works <kakimomokkuri@gmail.com>
2022-11-24 18:21:05 +0900

    refactor: 注文スキーマ

--- src/main/resources/db/migration/postgresql/V2.00__schema_refactor.sql
+++ src/main/resources/db/migration/postgresql/V2.00__schema_refactor.sql
@@ -9,6 +9,12 @@ CREATE TABLE auth.usr
     role_name  VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id)
 );
+COMMENT ON TABLE auth.usr IS 'ユーザー';
+COMMENT ON COLUMN auth.usr.user_id IS 'ユーザーID';
+COMMENT ON COLUMN auth.usr.first_name IS '姓';
+COMMENT ON COLUMN auth.usr.last_name IS '名';
+COMMENT ON COLUMN auth.usr.password IS 'パスワード';
+COMMENT ON COLUMN auth.usr.role_name IS 'ロール名';
 
 INSERT INTO auth.usr (user_id, first_name, last_name, password, role_name)
 SELECT user_id, first_name, last_name, password, role_name
