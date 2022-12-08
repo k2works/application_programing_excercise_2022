54809fc6c7d290a91af9952175ffe8d38c682000
k2works <kakimomokkuri@gmail.com>
2022-12-08 16:42:39 +0900

    refactor: パッケージの移動

--- src/test/java/bouquet/domain/model/auth/UserTest.java
+++ src/test/java/bouquet/domain/model/auth/UserTest.java
@@ -1,6 +1,5 @@
-package bouquet.domain.model;
+package bouquet.domain.model.auth;
 
-import bouquet.domain.model.auth.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
